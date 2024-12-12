04f549191b3d52b493cc2305a213db4f8b4a64f8
Refael Levi <refael14levi@gmail.com>
2024-12-13 00:11:03 +0200

    add LongTxt to reviews

--- vite-project/src/cmps/Accordion.jsx
+++ vite-project/src/cmps/Accordion.jsx
@@ -1,38 +1,16 @@
-import { useEffect, useState } from 'react'
-import { IoIosArrowForward, IoIosArrowDown  } from 'react-icons/io';
 
-export function Accordion({ children, title = 'Some important title' }) {
-    const [isMobile, setIsMobile] = useState(window.matchMedia("(max-width: 600px)").matches)
+
+export function Accordion({ title, children }) {
     const [isOpen, setIsOpen] = useState(false)
-
-    // useEffect(() => {
-    //     const mediaQuery = window.matchMedia("(max-width: 600px)")
-    //     const handleMediaChange = (event) => setIsMobile(event.matches)
-        
-    //     mediaQuery.addEventListener('change', handleMediaChange)
-
-    //     return () => {
-    //         mediaQuery.removeEventListener('change', handleMediaChange)
-    //     }
-    // }, [])
-
-    function toggleAccordion() {
-            setIsOpen((prev) => !prev)
-    }
-
+    const arrow = isOpen ? '▲' : '▼';
     return (
-
-            <section className={`accordion ${isOpen ? 'open' : ''}`}>
-                <section onClick={toggleAccordion} className="title-container">
-                    <h2>{title}</h2>
-                    <span className="arrow">
-                    {isOpen ? <IoIosArrowDown /> : <IoIosArrowForward />} 
-                </span>
-                    {/* </span> */} //! here (up and no down)
-                </section>
-                {isOpen && <section className="content">{children}</section>}
+        <section className="accordion">
+            <section onClick={() => setIsOpen(isOpen => !isOpen)} className="title-container">
+                <h2>{title}</h2>
+                <span>{arrow}</span>
             </section>
+            {isOpen && <div>{children}</div>}
+            <hr />
+        </section>
     )
-}
-
-// ⌄
+} 
--- vite-project/src/cmps/reviews/ReviewPreview.jsx
+++ vite-project/src/cmps/reviews/ReviewPreview.jsx
@@ -1,14 +1,7 @@
-import { useState } from "react"
+import { useState } from 'react'
+import { LongTxt } from '../LongTxt' 
 
 export function ReviewPreview({ reviewsToDisplay, MAX_LENGTH = 100 }) {
-  const [expanded, setExpanded] = useState(Array(reviewsToDisplay.length).fill(false))
-
-  const handleExpand = (index) => {
-      const newExpanded = [...expanded]
-      newExpanded[index] = !newExpanded[index]
-      setExpanded(newExpanded) //~ change to acordian better writing
-  } //! here acordion
-
   if (!reviewsToDisplay || !Array.isArray(reviewsToDisplay)) return <div>Loading...</div>
 
   return (
@@ -18,17 +11,11 @@ export function ReviewPreview({ reviewsToDisplay, MAX_LENGTH = 100 }) {
                   <div className="mini-user-details grid">
                       <img src={review.by.imgUrl} alt={review.by.fullname} className="mini-user-img" />
                       <p className="review-fullname">{review.by.fullname}</p>
-                      {/* <span>pen 2024</span> //! here createAt */}
-                      {/* <span className="review-createdAt">{reviewsToDisplay.createdAt}</span> */}
+                      <span>pen 2024</span> 
                   </div>
-                  <p>{expanded[index] ? review.txt : review.txt.substring(0, MAX_LENGTH)}</p>
-                  {review.txt.length > MAX_LENGTH &&
-                  <button className="show-more" onClick={() => handleExpand(index)}>
-                      {expanded[index] ? <><span>{'< '}</span><span className="underline">Show less</span> </> : <> <span className="underline">Show more</span> <span>{'>'}</span></>}
-                  </button>}
+                  <LongTxt txt={review.txt} length={MAX_LENGTH} />     
               </div>
-          ))
-          }
-      </div >
+          ))}
+      </div>
   )
 }
